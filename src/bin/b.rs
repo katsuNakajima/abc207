cbf4fe7f9aee80ef246d6e3a23e6c24a732beee8
katsuNakajima <40666782+katsuNakajima@users.noreply.github.com>
2021-10-02 14:55:17 +0900

    init

---
+++ src/bin/b.rs
@@ -0,0 +1,47 @@
+#[allow(unused_macros)]
+macro_rules! parse_line {
+    ( $t:ty ) => (
+        {
+            let mut line = String::new();
+            ::std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            iter.next().unwrap().parse::<$t>().unwrap()
+        }
+    );
+
+    ( $( $t:ty), +) => (
+        {
+            let mut line = String::new();
+            ::std::io::stdin().read_line(&mut line).unwrap();
+            let mut iter = line.split_whitespace();
+            ( $(iter.next().unwrap().parse::<$t>().unwrap()),* )
+        }
+    );
+}
+
+#[allow(unused_macros)]
+macro_rules! read_line {
+    () => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        line.pop();
+        line
+    }};
+}
+
+#[allow(unused_macros)]
+macro_rules! parse_vec {
+    ( $t:ty ) => {{
+        let mut line = String::new();
+        ::std::io::stdin().read_line(&mut line).unwrap();
+        let iter = line.split_whitespace();
+        iter.map(|v| v.parse::<$t>().unwrap()).collect::<Vec<_>>()
+    }};
+}
+
+fn main() {
+    let (a, b, c, d) = parse_line!(i32, i32, i32, i32);
+    let diff = c * d - b;
+    let ans = if diff <= 0 { -1 } else { (a + (diff - 1)) / diff };
+    println!("{}", ans);
+}
